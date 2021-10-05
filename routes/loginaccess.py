4379cd14f842d1a75b15d1fe89dbd8a79779a748
rickyred47 <89809802+rickyred47@users.noreply.github.com>
2021-10-05 12:15:57 -0400

    update database

--- routes/loginaccess.py
+++ routes/loginaccess.py
@@ -1,18 +1,30 @@
-from flask import render_template
+from flask import render_template, request
 
 
-def setup_page_routing(app):
+def setup_page_routing(app, base, db):
     # set up routes to run
 
+    # Database accounts
+    UserTable = base.classes.user
+    Error_MessageTable = base.classes.error_message
+
     # the login page route
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def login_page():
-        return render_template('login.html')
+        if request.method == "POST":
+            username = request.form["username_input"]
+            psswrd = request.form["password_input"]
+            errormessage = db.session.query(Error_MessageTable).filter_by(id=5).first()
+            print(errormessage.message)
+            return render_template('login.html', errormessage=errormessage.message)
+        else:
+            errormessage = db.session.query(Error_MessageTable).filter_by(id=0).first()
+            return render_template('login.html', errormessage=errormessage.message)
 
     @app.route('/fgtpsswrd')
     def forgot_password():
         return render_template('forgot_password.html')
 
-    @app.route('/newuser')
+    @app.route('/new_user')
     def new_user():
         return render_template('new_user.html')
--- testingheroku.py
+++ testingheroku.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Futureime21"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vykjdeiaptubch:32eca6f9f825d0478014c320daeb3347bc5b50226af10ae619b47c02dbae36da@ec2-44-199-26-122.compute-1.amazonaws.com:5432/df993q19u6tf7h'
+db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
 
 from routes import loginaccess
 
-loginaccess.setup_page_routing(app)
+loginaccess.setup_page_routing(app, Base, db)
 
 if __name__ == '__main__':
     app.run()
