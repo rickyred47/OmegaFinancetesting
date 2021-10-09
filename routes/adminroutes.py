86004d1b419f1718947dc354c319f076b473c7d9
rickyred47 <89809802+rickyred47@users.noreply.github.com>
2021-10-09 13:32:42 -0400

    updated added the admin Page

---
+++ routes/adminroutes.py
@@ -0,0 +1,10 @@
+from flask import render_template, redirect, request
+
+
+def setup_page_routing(app, base, db):
+    # set up routes to run
+
+    # The admin pages
+    @app.route('/admin')
+    def admin_home_page():
+        return render_template('admin.html')
