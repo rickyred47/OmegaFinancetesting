9dea2a9964c81599e310058562f5faf5d5574e3b
Jade <popkilj289@gmail.com>
2021-11-12 12:12:39 -0500

    Made sure journal event was only made if a valid status was supplied

--- routes/managerroutes.py
+++ routes/managerroutes.py
@@ -43,7 +43,7 @@ def setup_page_routing(app, database):
 
     @app.route('/manager/<account_number>_to_id')
     def manager_number_id(account_number):
-        if"Manager" in session:
+        if "Manager" in session:
             account = database.get_account_info_by_number(account_number)
             return redirect(url_for('manager_account_ledger', account_id=account.id))
         else:
@@ -89,16 +89,17 @@ def setup_page_routing(app, database):
                     database.commit_info()
                     modified = True
                 # Create journal event entry
-                journal_event = database.get_journal_event_table()
-                new_entry2 = journal_event(journal_type_before=entry.type, journal_type_after=entry.type,
-                                           journal_debit_accounts_before=entry.debit_accounts, journal_debit_accounts_after=entry.debit_accounts,
-                                           journal_debit_amounts_before=entry.debit_amounts, journal_debit_amounts_after=entry.debit_amounts,
-                                           journal_credit_accounts_before=entry.credit_accounts, journal_credit_accounts_after=entry.credit_accounts,
-                                           journal_credit_amounts_before=entry.credit_amounts, journal_credit_amounts_after=entry.credit_amounts,
-                                           journal_status_before='Pending',
-                                           journal_status_after='Accepted' if request.form['accept_reject'] == 'Accept' else 'Rejected',
-                                           date_made=datetime.now(), event_type='Modified', username=session['username'])
-                database.commit_to_database(new_entry2)
+                if modified:
+                    journal_event = database.get_journal_event_table()
+                    new_entry2 = journal_event(journal_type_before=entry.type, journal_type_after=entry.type,
+                                               journal_debit_accounts_before=entry.debit_accounts, journal_debit_accounts_after=entry.debit_accounts,
+                                               journal_debit_amounts_before=entry.debit_amounts, journal_debit_amounts_after=entry.debit_amounts,
+                                               journal_credit_accounts_before=entry.credit_accounts, journal_credit_accounts_after=entry.credit_accounts,
+                                               journal_credit_amounts_before=entry.credit_amounts, journal_credit_amounts_after=entry.credit_amounts,
+                                               journal_status_before='Pending',
+                                               journal_status_after='Accepted' if request.form['accept_reject'] == 'Accept' else 'Rejected',
+                                               date_made=datetime.now(), event_type='Modified', username=session['username'])
+                    database.commit_to_database(new_entry2)
                 return redirect(url_for('manager_journal'))
             else:
                 return '', 204
