f3c08a74317c725c2b7ef3d225d91887609a98aa
Jade <popkilj289@gmail.com>
2021-11-12 12:05:07 -0500

    Added journal event creation on journal entry approval / rejection

--- routes/managerroutes.py
+++ routes/managerroutes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, session, redirect, url_for
+from datetime import datetime
 from obj import journal
 
 
@@ -78,12 +79,26 @@ def setup_page_routing(app, database):
             if request.method == "POST":
                 entry_id = request.form["entry_id"]
                 entry = database.get_journal_entry(entry_id)
+                modified = False
                 if request.form['accept_reject'] == 'Accept':
                     entry.status = "Accepted"
                     database.commit_info()
+                    modified = True
                 elif request.form['accept_reject'] == 'Reject':
                     entry.status = "Rejected"
                     database.commit_info()
+                    modified = True
+                # Create journal event entry
+                journal_event = database.get_journal_event_table()
+                new_entry2 = journal_event(journal_type_before=entry.type, journal_type_after=entry.type,
+                                           journal_debit_accounts_before=entry.debit_accounts, journal_debit_accounts_after=entry.debit_accounts,
+                                           journal_debit_amounts_before=entry.debit_amounts, journal_debit_amounts_after=entry.debit_amounts,
+                                           journal_credit_accounts_before=entry.credit_accounts, journal_credit_accounts_after=entry.credit_accounts,
+                                           journal_credit_amounts_before=entry.credit_amounts, journal_credit_amounts_after=entry.credit_amounts,
+                                           journal_status_before='Pending',
+                                           journal_status_after='Accepted' if request.form['accept_reject'] == 'Accept' else 'Rejected',
+                                           date_made=datetime.now(), event_type='Modified', username=session['username'])
+                database.commit_to_database(new_entry2)
                 return redirect(url_for('manager_journal'))
             else:
                 return '', 204
