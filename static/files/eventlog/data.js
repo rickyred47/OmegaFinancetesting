4b42975dd26ac02ad12949881c32227431615993
Aaron Moore <57806727+InverseDigital@users.noreply.github.com>
2021-11-16 13:00:32 -0500

    Fixed Journal table running off the page by adding a scroll fuctionality.

--- static/files/eventlog/data.js
+++ static/files/eventlog/data.js
@@ -1,5 +1,5 @@
 ﻿function filter() {
-  var table, tr, i, td1, txtValueDate;
+  var table, tr, i, td, txtValueDate;
   var date_start = document.getElementById("start_date").value;
   var date_end = document.getElementById("end_date").value;
   const start_date = new Date(date_start.substr(0,4), date_start.substr(5,2), date_start.substr(8,2));
@@ -7,14 +7,16 @@
   table = document.getElementById("events_table");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td1 = tr[i].getElementsByTagName("td")[1];
-    txtValueDate = td1.textContent || td1.innerText;
-    const date = new Date(txtValueDate.substr(0,4), txtValueDate.substr(5,2), txtValueDate.substr(8,2));
-    if (start_date <= date && date <= end_date) {
-        tr[i].style.display = "";
-        }
-    else {
-        tr[i].style.display = "none";
+    td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValueDate = td.textContent || td.innerText;
+            const date = new Date(txtValueDate.substr(0,4), txtValueDate.substr(5,2), txtValueDate.substr(8,2));
+            if ( start_date <= date && date <= end_date) {
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+            }
         }
     }
-  }
+}
